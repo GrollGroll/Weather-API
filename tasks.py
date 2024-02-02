c144d73318df6693d9fe3a5847fdf1d57487f025
Anton <golovanov.2000@mail.ru>
2024-02-02 14:11:08 +0300

    First task is done

--- tasks.py
+++ tasks.py
@@ -1,5 +1,32 @@
+import json
+from external.client import YandexWeatherAPI
+from utils import get_url_by_city_name, CITIES
+
+
 class DataFetchingTask:
-    pass
+    def __init__(self, city_name: str, cities_dict: dict) -> None:
+        self.city_name = city_name
+        self.cities_dict = cities_dict
+
+    def get_url(self, city_name: str) -> dict:
+        url_with_data = get_url_by_city_name(city_name)
+        try:
+            resp = YandexWeatherAPI.get_forecasting(url_with_data)
+            return resp
+        except Exception as e:
+            print(f"Error fetching data for {city_name}: {e}")
+            return None
+        
+    def create_json(self, cities_dict: dict) -> None:
+        all_data = []
+
+        for city in cities_dict:
+            data_dict = self.get_url(city)
+            if data_dict is not None:
+                all_data.append(data_dict)
+
+        with open('all_cities_data.json', 'w') as file:
+            json.dump(all_data, file, indent=2) 
 
 
 class DataCalculationTask:
@@ -12,3 +39,8 @@ class DataAggregationTask:
 
 class DataAnalyzingTask:
     pass
+
+
+
+
+
