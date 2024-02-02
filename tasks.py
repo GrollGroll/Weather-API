4a3f66b9d1e480158623f4e308856068f360b2cf
Anton <golovanov.2000@mail.ru>
2024-02-02 14:39:02 +0300

    First task is done

--- tasks.py
+++ tasks.py
@@ -2,13 +2,13 @@ import json
 from external.client import YandexWeatherAPI
 from utils import get_url_by_city_name, CITIES
 
-
 class DataFetchingTask:
     def __init__(self, city_name: str, cities_dict: dict) -> None:
         self.city_name = city_name
         self.cities_dict = cities_dict
 
-    def get_url(self, city_name: str) -> dict:
+    @staticmethod
+    def get_url(city_name: str) -> dict:
         url_with_data = get_url_by_city_name(city_name)
         try:
             resp = YandexWeatherAPI.get_forecasting(url_with_data)
@@ -16,17 +16,17 @@ class DataFetchingTask:
         except Exception as e:
             print(f"Error fetching data for {city_name}: {e}")
             return None
-        
-    def create_json(self, cities_dict: dict) -> None:
-        all_data = []
 
-        for city in cities_dict:
-            data_dict = self.get_url(city)
-            if data_dict is not None:
-                all_data.append(data_dict)
+    def create_json(self, filename: str = 'all_cities_data.json') -> None:
+        with open(filename, 'w') as file:
+            data_generator = (self.get_url(city) for city in self.cities_dict)
+            filtered_data = filter(None, data_generator)
+            json.dump(list(filtered_data), file, indent=2)
 
-        with open('all_cities_data.json', 'w') as file:
-            json.dump(all_data, file, indent=2) 
+
+task = DataFetchingTask(city_name='Moscow', cities_dict=CITIES)
+task.create_json()
+
 
 
 class DataCalculationTask:
